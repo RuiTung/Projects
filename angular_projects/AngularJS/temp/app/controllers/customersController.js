7743a2c803e0a7cd3905cef96e439b424ddf9c9c
RuiTung <ruimavic@gmail.com>
2020-08-09 17:58:42 -0700

    Angular-factory
    Add factory obj for reuseable code

--- angular_projects/AngularJS/temp/app/controllers/customersController.js
+++ angular_projects/AngularJS/temp/app/controllers/customersController.js
@@ -40,80 +40,22 @@
 
 // option 3
 (function() {
-    var CustomersController = function ($scope) {
+    var CustomersController = function ($scope, customersFactory) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
+        $scope.customers = [];
 
-        $scope.customers = [
-            {
-                id: 1,
-                joined: '2020,-01-01',
-                name: 'Abel',
-                city: 'Van',
-                orderTotal: 3.86,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'shoes',
-                        total: 3.86
-                    }
-                ]
-            },
-            {
-                id: 2,
-                joined: '2018-08-27',
-                name: 'Billy',
-                city: 'Van',
-                orderTotal: 11.38,
-                orders: [
-                    {
-                        id: 2,
-                        product: 'candy',
-                        total: 3.38
-                    },
-                    {
-                        id: 3,
-                        product: 'bat',
-                        total: 8
-                    }
-                ]
-            },
-            {
-                id: 3,
-                joined: '2020-08-21',
-                name: 'Zoe',
-                city: 'Vic',
-                orderTotal: 101,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'bottle',
-                        total: 101
-                    }
-                ]
-            }, 
-            {
-                id: 4,
-                joined: '1944-01-01',
-                name: 'Tu',
-                city: 'Ab',
-                orderTotal: 88888231.21,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'bag',
-                        total: 88888231.21
-                    }
-                ]
-            }
-        ];
+        function init() {
+            $scope.customers = customersFactory.getCustomers();
+        }
+        init();
 
         $scope.doSort = function(propName) {
             $scope.sortBy = propName;
             $scope.reverse = !$scope.reverse;
         };
     };
-    CustomersController.$inject = ['$scope'];
+    CustomersController.$inject = ['$scope', 'customersFactory'];
     angular.module('customersApp')
         .controller('CustomersController', CustomersController);
 
--- angular_projects/AngularJS/temp/app/controllers/ordersController.js
+++ angular_projects/AngularJS/temp/app/controllers/ordersController.js
@@ -1,87 +1,19 @@
 (function() {
-    var OrdersController = function ($scope, $routeParams) {
+    var OrdersController = function ($scope, $routeParams, customersFactory) {
         var customerId = $routeParams.customerId;
-        $scope.orders = null;
+        $scope.customer = null;
 
         function init() {
-            for(var i = 0, len = $scope.customers.length; i < len; i++) {
-                if($scope.customers[i].id === parseInt(customerId)) {
-                    $scope.orders = $scope.customers[i].orders;
-                    break;
-                }
-            }
+            $scope.customer = customersFactory.getCustomer(customerId);
         }
 
-        $scope.customers = [
-            {
-                id: 1,
-                joined: '2020,-01-01',
-                name: 'Abel',
-                city: 'Van',
-                orderTotal: 3.86,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'shoes',
-                        total: 3.86
-                    }
-                ]
-            },
-            {
-                id: 2,
-                joined: '2018-08-27',
-                name: 'Billy',
-                city: 'Van',
-                orderTotal: 11.38,
-                orders: [
-                    {
-                        id: 2,
-                        product: 'candy',
-                        total: 3.38
-                    },
-                    {
-                        id: 3,
-                        product: 'bat',
-                        total: 8
-                    }
-                ]
-            },
-            {
-                id: 3,
-                joined: '2020-08-21',
-                name: 'Zoe',
-                city: 'Vic',
-                orderTotal: 101,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'bottle',
-                        total: 101
-                    }
-                ]
-            }, 
-            {
-                id: 4,
-                joined: '1944-01-01',
-                name: 'Tu',
-                city: 'Ab',
-                orderTotal: 888231.21,
-                orders: [
-                    {
-                        id: 1,
-                        product: 'bag',
-                        total: 888231.21
-                    }
-                ]
-            }
-        ];
         init();
         $scope.doSort = function(propName) {
             $scope.sortBy = propName;
             $scope.reverse = !$scope.reverse;
         };
     };
-    OrdersController.$inject = ['$scope', '$routeParams'];
+    OrdersController.$inject = ['$scope', '$routeParams', 'customersFactory'];
     angular.module('customersApp')
         .controller('OrdersController', OrdersController);
 
