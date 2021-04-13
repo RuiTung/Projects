5e6678daf3bfda25c8df4ed4f6b38ca954113f99
RuiTung <ruimavic@gmail.com>
2021-04-12 19:22:31 -0700

    C# Basic
    C# Basic

---
+++ C#_projects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C__projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+                System.Console.WriteLine(b);
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
