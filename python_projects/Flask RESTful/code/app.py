e425e8691fad06ea5c23d8f1af89655dcdbe7e93
RuiTung <ruimavic@gmail.com>
2021-04-25 16:27:17 -0700

    Store with Flask-SQLAlchemy
    Store with Flask-SQLAlchemy

--- python_projects/Flask RESTful/code/app.py
+++ python_projects/Flask RESTful/code/app.py
@@ -3,18 +3,31 @@ from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from user import UserRegister
-from item import Item, ItemList
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.secret_key = 'dover'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.secret_key = "dover"
 api = Api(app)
+
+# Will run line 11 to create a new database before the first request
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
 
-if __name__ == '__main__': # to avoid running without python app.py
+if __name__ == "__main__": # to avoid running without python app.py
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
---
+++ python_projects/Flask RESTful/code/models/item.py
@@ -0,0 +1,38 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+    
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
+    store = db.relationship("StoreModel")
+
+    def __init__(self, name, price, store_id):
+        self.name = name
+        self.price = price
+        self.store_id = store_id
+
+
+    def json(self):
+        return {"name": self.name, "price": self.price}
+    
+    @classmethod
+    def find_by_name(cls, name):
+        # return an ItemModel object
+        # return ItemModel.query.filter_by(name=name) # means SELECT * FROM items WHERE name=name
+        return cls.query.filter_by(name=name).first() # means SELECT * FROM items WHERE name=name LIMITE 1
+
+    # for both update and insert
+    def save_to_db(self):
+        # session is a collection of object that we want to write into database
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+            
+    
